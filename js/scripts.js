4b3f06653d4bfbefb84b4ac48a67222d6d8bb029
Anita Kemp and Christopher Burge <anita@>
2019-08-19 16:10:10 -0700

    Add card deck

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
-  $('#submit').click(function(event) {
-    var sent = $("textarea#sentence").val();
-
-    var test = [];
-    var sents = sent.split(' ');
-    sents.forEach(function(sent){
-      test = sents.filter(word=>word.length>=3);
-
+  // $('#submit').click(function(event) {
+    var numbers = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+    var suits = ['hearts', 'spades', 'diamonds', 'clubs'];
+    var deck = [];
+    suits.forEach(function(suit) {
+      numbers.forEach(function(number) {
+        deck.push(number + " of " + suit);
+        console.log(number + " of " + suit);
+      });
     });
 
-    var reversing = test.reverse();
-    var newSentence= reversing.join();
-    alert(newSentence);
 
+
+
+  });
+
+  // });
+// });
+
+////////////////////Word Play/////////////////////////////
+    // var sent = $("textarea#sentence").val();
+    //
+    // var test = [];
+    // var sents = sent.split(' ');
+    // sents.forEach(function(sent){
+    //   test = sents.filter(word=>word.length>=3);
+    //
+    // });
+    //
+    // var reversing = test.reverse();
+    // var newSentence= reversing.join();
+    // alert(newSentence);
+/////////////////////Grocery List///////////////////////////////
     // var items = ['item1', 'item2', 'item3', 'item4', 'item5'];
     //
     // items.map(function(items) {
@@ -21,6 +40,5 @@ $(document).ready(function() {
     // $('ul').append(finalList);
 
 
-    });
-  });
+
 // });
