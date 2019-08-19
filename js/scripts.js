531e39b031d669f5475722f106e377263a292ebd
Anita Kemp and Christopher Burge <christopher@>
2019-08-19 14:47:25 -0700

    create a for loop that gets characters more than 3 and reverse them into new array

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
   $('#submit').click(function(event) {
-    var items = ['item1', 'item2', 'item3', 'item4', 'item5'];
+    var sent = $("textarea#sentence").val();
 
-    items.map(function(items) {
-    var groceryList = $('input#' + items).val();
-    var finalList = groceryList.toUpperCase();
-    $('ul').append(finalList);
+    var test = [];
+    var sents = sent.split(' ');
+    sents.forEach(function(sent){
+      test = sents.filter(word=>word.length>=3);
+
+    });
+
+    var reversing = test.reverse();
+    var newSentence= reversing.join();
+    alert(newSentence);
+
+    // var items = ['item1', 'item2', 'item3', 'item4', 'item5'];
+    //
+    // items.map(function(items) {
+    // var groceryList = $('input#' + items).val();
+    // var finalList = groceryList.toUpperCase();
+    // $('ul').append(finalList);
 
 
     });
   });
-});
+// });
